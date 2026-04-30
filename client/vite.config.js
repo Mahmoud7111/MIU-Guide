c38a9b391222d6c4792484671f335b51d5b05299
Mahmoud Sayed <mahmoudsyd24@gmail.com>
2026-04-30 11:43:54 +0300

    feat: add vercel.json for routing configuration; update vite.config.js for build settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,4 +12,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
